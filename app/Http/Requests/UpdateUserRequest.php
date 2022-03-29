bb659fe801df51ac72a7fb570194694363625358
Wasim Ahmad <wasimahmad2520@gmail.com>
2022-03-29 22:14:00 +0500

    first commit

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * File name: UpdateUserRequest.php
+ * Last modified: 2021.06.29 at 11:59:29
+ * Author: SmarterVision - https://codecanyon.net/user/smartervision
+ * Copyright (c) 2021
+ */
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        User::$rules['email'] = 'required|max:255|email|unique:users,email,' . $this->route('user');
+        User::$rules['phone_number'] = 'required|max:255|unique:users,phone_number,' . $this->route('user');
+        User::$rules['password'] = 'nullable';
+        return User::$rules;
+    }
+}
